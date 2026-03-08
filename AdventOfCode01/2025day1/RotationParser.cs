26def871ba2c29523aee88d4caedfe2b86d01fca
RusandeKodare <o.p.j@hotmail.com>
2026-03-08 15:22:48 +0100

    2025Day1Part2
    Move files
    Implement logic to count all hits on zero
    Create new class to keep track of zeroes
    Create bool to be able to turn off/on CountAllZeroes

---
+++ AdventOfCode01/2025day1/RotationParser.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode01;
+
+public static class RotationParser
+{
+    public static int ParseRotation(string item)
+    {
+        var digitWeNeed = new string(item.Skip(1).ToArray());
+        var convertedDigit = int.Parse(digitWeNeed);
+        return convertedDigit;
+    }
+}
---
+++ AdventOfCode01/Interfaces/IUI.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode01.Interfaces;
+
+public interface IUI
+{
+    public void Write(string output);
+    public string Read();
+}
